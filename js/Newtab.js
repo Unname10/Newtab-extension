c4b0b77f018293dd2aff5e64e6575c2a347b78c1
TiGb8002 <chautai23022008@gmail.com>
2022-04-12 21:21:59 +0700

    Hoàn thành phần Background

--- js/Newtab.js
+++ js/Newtab.js
@@ -1,4 +1,4 @@
-var images = ['bg-02.jpg', 'bg-03.jpg', 'bg-08.jpg', 'bg-10.jpg', 'bg-11.jpg']
+var images = ['bg-02.jpg', 'bg-03.jpg', 'bg-08.jpg', 'bg-10.jpg']
 function changeRandomImage(e) {
     let i = Math.floor((Math.random() * images.length));
     e.style.backgroundImage = `url("../images/bg/${images[i]}")`;
@@ -8,14 +8,25 @@ window.onload = function () {
     var bgElement = document.querySelector("#bg");
     var bgElement2 = document.querySelector("#bg2");
     changeRandomImage(bgElement);
+    changeRandomImage(bgElement2);
     setTimeout(() => {
         bgElement.classList.remove("fadeIn");
     }, 1500)
     setInterval(() => {   
-        changeRandomImage(bgElement)
-        bgElement.classList.add("fadeIn")
+        if (bgElement.style.display == "none"){
+            bgElement.style.display = "block";
+            bgElement2.style.display = "none";
+            bgElement.classList.add("fadeIn");
+            changeRandomImage(bgElement2);
+        } else{
+            bgElement.style.display = "none";
+            bgElement2.style.display = "block";
+            bgElement2.classList.add("fadeIn");
+            changeRandomImage(bgElement);
+        }
         setTimeout(() => {
             bgElement.classList.remove("fadeIn");
+            bgElement2.classList.remove("fadeIn");
         }, 1500)
     }, 5000);
 }
