17edf3cfc3e5ae43a566daa97cd6cc006e0473c1
TiGb8002 <chautai23022008@gmail.com>
2022-05-04 10:16:56 +0700

    Xong các chức năng trong Sidebar

--- js/Newtab.js
+++ js/Newtab.js
@@ -1,4 +1,4 @@
-// Function
+// Global Function
 function changeRandomImage(element) {
     let i = Math.floor((Math.random() * 24))+1;
     element.style.backgroundImage = `url("../images/bg/${twoDigitNumber(i)}.png")`;
@@ -23,10 +23,16 @@ function changeBg(element1, element2, time) {
     }, time)
 }
 
+function toggle(element, status) {
+    if (status) element.style.display = "flex";
+    else element.style.display = "none";
+}
+
 window.onload = async function () {
     //Local Function
     function closeSidebar() {
         sidebar.style.left = `-${sidebar.offsetWidth}px`;
+        addDialog.style.display = "none";
         for (let i of [bgElement, bgElement2]) {
             i.removeEventListener("click", closeSidebar);
         }
@@ -44,6 +50,33 @@ window.onload = async function () {
         openSidebarBtn.classList.toggle("dark-content");
     }
 
+    function addContent(element, storageData, onError) {
+        if (storageData.length === 0){
+            let alertMess = document.createElement("p");
+            alertMess.classList.add("alert-content");
+            alertMess.innerText = onError;
+            element.appendChild(alertMess);   
+        } else
+        for (let i of storageData){
+            let subContent = document.createElement("div");
+            subContent.classList.add("subs-content");
+
+            let iconContent = document.createElement("img");
+            iconContent.classList.add("icons-content");
+            iconContent.src = `https://s2.googleusercontent.com/s2/favicons?domain_url=${i.url}`;
+
+            let textContent = document.createElement("a");
+            textContent.classList.add("texts-content");
+            textContent.innerText = i.title;
+            textContent.href = i.url;
+
+            subContent.appendChild(iconContent);
+            subContent.appendChild(textContent);
+
+            element.appendChild(subContent);
+        }
+    }
+
     //Variable
     var storageData = await chrome.storage.sync.get();
     var bgElement = document.querySelector("#bg");
@@ -54,6 +87,12 @@ window.onload = async function () {
     var openSidebarBtn = document.querySelector(".open-sidebar");
     var closeSidebarBtn = document.querySelector(".fa-arrow-left");
     var recentElement = document.querySelector(".recently");
+    var favoriteElement = document.querySelector(".favorite");
+    var bookmarkElement = document.querySelector(".bookmark");
+    var recentContentElement = document.querySelector(".recent-content");
+    var favoriteContentElement = document.querySelector(".favorite-content");
+    var bookmarkContentElement = document.querySelector(".bookmark-content");
+    console.log(storageData)
 
     //Execute
     changeRandomImage(bgElement);
@@ -90,9 +129,78 @@ window.onload = async function () {
         switchMode();
         chrome.storage.sync.set({"darkMode":darkmodeSwitch.checked})
     }
+    
+    addContent(recentContentElement, storageData.recent, "Bạn chưa truy cập trang nào gần đây!");
+    addContent(favoriteContentElement, storageData.favorite, "Bạn chưa truy cập trang nào gần đây!");
 
-    // recentElement.onmouseenter = () => {
-        
-    // }
+    let subContentElement = `
+        <div class="add-content-dialog">
+            <div class="dialog-items dialog-header">
+                <h2 class="dialog-header">Thêm Bookmark:</h3>
+                <i class="fa-solid fa-plus"></i>
+            </div>
+            <div class="form__group url-block">
+                <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+                <label for="name" class="form__label">URL trang Web:</label>
+            </div>
+            <div class="form__group title-block">
+                <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+                <label for="name" class="form__label">Tiêu đề:</label>
+            </div>
+        </div>
+        <div class="subs-content add-content">
+            <i class='bx bx-add-to-queue'></i>
+            <p class="add-content-title">Thêm Bookmark</p>
+        </div>
+    `;
+    if (storageData.bookmark.length === 0)
+        subContentElement += `
+            <p class="alert-content">Bạn chưa thêm Bookmark nào!</p>
+        `;
+    else {
+        for (let i = 0; i < storageData.bookmark.length; ++i){
+            subContentElement += `
+                <div class="subs-content">
+                    <img class="icons-content" src="https://s2.googleusercontent.com/s2/favicons?domain_url=${storageData.bookmark[i].url}"/>
+                    <a class="texts-content" href="${storageData.bookmark[i].url}">${storageData.bookmark[i].title}</a>
+                    <i class="fa-solid fa-trash" data-id="${i}"></i>
+                </div>
+            `    
+        }
+    }
+    bookmarkContentElement.innerHTML = subContentElement;
 
+    var trashIcon = document.querySelectorAll(".fa-trash");
+    for (let i of trashIcon) {
+        i.onclick = () => {
+            storageData.bookmark.splice(i.dataset.id, 1);
+            chrome.storage.sync.set({"bookmark": storageData.bookmark})
+        }
+    }
+
+    var addELement = document.querySelector(".add-content");
+    var addDialog = document.querySelector(".add-content-dialog");
+    addELement.onclick = () => {
+        addDialog.style.display = "flex";
+        var addBtn = document.querySelector(".fa-plus")
+        var inputField = document.querySelectorAll(".form__field")
+        addBtn.onclick = () => {
+            if (!inputField[0].value){
+                alert("Bạn chưa nhập URL trang Web!");
+            } else{
+                storageData.bookmark.push({
+                    "url": inputField[0].value,
+                    "title": (inputField[1].value) ? inputField[1].value : inputField[0].value
+                })
+                chrome.storage.sync.set({"bookmark": storageData.bookmark});
+            }
+        }
+    }
+    
+    recentElement.addEventListener("mouseenter", () => toggle(recentContentElement, true));
+    recentElement.addEventListener("mouseleave", () => toggle(recentContentElement, false));
+    favoriteElement.addEventListener("mouseenter", () => toggle(favoriteContentElement, true));
+    favoriteElement.addEventListener("mouseleave", () => toggle(favoriteContentElement, false));
+    bookmarkElement.addEventListener("mouseenter", () => toggle(bookmarkContentElement, true));
+    bookmarkElement.addEventListener("mouseleave", () => toggle(bookmarkContentElement, false));
 }
--- js/background.js
+++ js/background.js
@@ -3,14 +3,67 @@ chrome.runtime.onInstalled.addListener(() => {
         "animationDuration": "1.5",
         "bgChangeTime": 5,
         "darkMode": true,
-        "recent": {},
-        "favorite":{}
+        "recent": [],
+        "favorite": [],
+        "bookmark": [
+            {
+                title: "Youtube",
+                url: "https://www.youtube.com/"
+            },
+            {
+                title: "MyAnimeList.net - Anime and Manga Database and Community",
+                url: "https://myanimelist.net/"
+            },
+            {
+                title: "Xem Phim Anime - Hoạt Hình Trung Quốc Hay Online || AnimeHay",
+                url: "https://animehay.club/"
+            },
+            {
+                title: "Facebook",
+                url: "https://www.facebook.com/"
+            }
+        ]
     });
 })
 
 
-chrome.runtime.onMessage.addListener((mess, sender) => {
-    console.log(sender.tab.url);
-    console.log(sender.tab.favIconUrl);
-    console.log(sender.tab.title);
+chrome.runtime.onMessage.addListener(async (mess, sender) => {
+    let dataStorage = await chrome.storage.sync.get();
+    let recentArrStoraged = dataStorage.recent;
+    let favoriteArrStoraged = dataStorage.favorite;
+    let indexOfRecent = recentArrStoraged.findIndex(page => {return page.url == sender.tab.url});
+    let indexOfFavorite = favoriteArrStoraged.findIndex(page => {return page.url == sender.tab.url});
+    let pageInfo = {
+        "url": sender.tab.url,
+        "title": sender.tab.title
+    }
+
+    if (indexOfRecent == -1) {
+        recentArrStoraged.unshift(pageInfo);
+    } else {
+        recentArrStoraged.splice(indexOfRecent, 1);
+        recentArrStoraged.unshift(Object.assign(pageInfo));
+    }
+    if (recentArrStoraged.length > 10) recentArrStoraged.pop();
+
+
+    if (indexOfFavorite == -1)
+        favoriteArrStoraged.unshift({
+            ...pageInfo,
+            "counter": 1
+        });
+    else {
+        favoriteArrStoraged[indexOfFavorite].counter++;
+        favoriteArrStoraged.sort((a,b) => {
+            if (a.counter < b.counter) return -1;
+            else if (a.counter > b.counter) return 1;
+            return 0;
+        })
+    }
+    if (favoriteArrStoraged.length > 10) favoriteArrStoraged.pop()
+
+    chrome.storage.sync.set({
+        "recent": recentArrStoraged,
+        "favorite": favoriteArrStoraged
+    })
 })
